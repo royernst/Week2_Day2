200a60d383872e5fc7f653552cc3db6a2d89f1df
royernst <roy.e31@hotmail.com>
2017-10-03 10:30:52 -0400

    Initial commit.

---
+++ Week2_Day2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2_Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int arraySize = int.Parse(Console.ReadLine());
+            //int[] newArray = new int[arraySize];
+            //Console.WriteLine("The array is of length " + newArray.Length);
+
+            string[] birthMonths = { "October", "July", "October", "May" };
+            Console.WriteLine(Array.IndexOf(birthMonths, "May"));
+
+            int[] numbers = { 2, 5, 9, 4, 3, 4, 6, 5 };
+            Console.WriteLine(Array.LastIndexOf(numbers, 4));
+
+            Array.Reverse(numbers);
+
+            Console.WriteLine(numbers[0]);
+        }
+    }
+}
