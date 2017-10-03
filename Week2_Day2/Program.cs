e952b2a3ff99de3673d780900e8107983e5d31cf
royernst <roy.e31@hotmail.com>
2017-10-03 11:37:45 -0400

    Finished up arrays, about to start loops.

--- Week2_Day2/Program.cs
+++ Week2_Day2/Program.cs
@@ -14,15 +14,35 @@ namespace Week2_Day2
             //int[] newArray = new int[arraySize];
             //Console.WriteLine("The array is of length " + newArray.Length);
 
-            string[] birthMonths = { "October", "July", "October", "May" };
-            Console.WriteLine(Array.IndexOf(birthMonths, "May"));
+            //string[] birthMonths = { "October", "July", "October", "May" };
+            //Console.WriteLine(Array.IndexOf(birthMonths, "May"));
 
-            int[] numbers = { 2, 5, 9, 4, 3, 4, 6, 5 };
-            Console.WriteLine(Array.LastIndexOf(numbers, 4));
+            //int[] numbers = { 2, 5, 9, 4, 3, 4, 6, 5 };
+            //Console.WriteLine(Array.LastIndexOf(numbers, 4));
 
-            Array.Reverse(numbers);
+            //Array.Reverse(numbers);
+            //Console.WriteLine(numbers[0]);
 
-            Console.WriteLine(numbers[0]);
+            //Array.Sort(numbers);
+            //Console.WriteLine(numbers[0]);
+            //Console.WriteLine(numbers[numbers.Length - 1]);
+
+            //string[] names = { "John", "Paul", "George", "Ringo:" };
+            //Console.WriteLine(Array.IndexOf(names, "John"));
+
+            //int[] luckyNumbers = { 4, 3, 2, 1, 4, 3, 2, 1 };
+            //Console.WriteLine(Array.LastIndexOf(luckyNumbers, 4));
+
+            //char[] alphabet = { 'a', 'b', 'c', 'd' };
+            //Console.WriteLine(alphabet[0]);
+            //Array.Reverse(alphabet);
+            //Console.WriteLine(alphabet[0]);
+
+            //string[] students = { "Roy", "JD", "Brian", "Kaitie", "Raz", "Monica" };
+            //Array.Sort(students);
+            //Console.WriteLine(students[0] + "  \n" + students[5]);
+
+
         }
     }
 }
